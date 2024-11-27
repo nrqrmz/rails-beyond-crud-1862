38cd6146343a9f49bf22cc99356969f7c9bc3408
Enrique Ramirez <nrqrmz@gmail.com>
2024-11-27 09:12:08 -0600

    finish lecture

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source "https://rubygems.org"
 
 gem "simple_form"
-
+gem "faker"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.2.2"
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :restaurants do
+    collection do
+      get :top
+    end
+
+    member do
+      get :chef
+    end
+
+    resources :reviews, only: %i[new create] # index, new, create
+  end
+
+  resources :reviews, only: :destroy # show, edit, update, destroy
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
